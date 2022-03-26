8669bb99543f74e890ab0d718329135d35e95076
Giorgio Savastano <giorgiosavastano@gmail.com>
2022-03-26 13:51:47 +0000

    added tests (#9)
    Co-authored-by: Giorgio Savastano <giorgio.savastano@uniroma1.it>

--- tests/test_classification_pipeline.py
+++ tests/test_classification_pipeline.py
@@ -4,7 +4,11 @@ from unittest import TestCase
 import numpy as np
 import pytest
 
-from cassa.classification_pipeline import get_affinity_matrix
+from cassa.classification_pipeline import (
+    eigen_decomposition,
+    get_affinity_matrix,
+    get_clusters_spectral,
+)
 from cassa.distance_matrix import compute_distance_matrix, compute_distance_matrix_dask
 
 path = Path(__file__)
@@ -25,6 +29,14 @@ class TestDistMatrix(TestCase):
 
         aff_matrix = get_affinity_matrix(dist_matr)
         self.assertEqual(aff_matrix.shape, dist_matr.shape)
+
+        n_cl = eigen_decomposition(aff_matrix)[0]
+        self.assertGreater(n_cl, 0)
+
+        l_labels, cl_colors, clusterer = get_clusters_spectral(
+            dist_matr, ncl=n_cl, self_tuned=True
+        )
+        self.assertEqual(len(l_labels), len(cl_colors))
 
     def tearDown(self):
         pass
