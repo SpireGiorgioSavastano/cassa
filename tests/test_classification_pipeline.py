e6a2361e3355b93ec317e62ee6abd7f3bdd972bf
Karl Nordstrom <93078172+karl-nordstrom@users.noreply.github.com>
2022-03-27 17:53:50 +0100

    Removing dask dependency and updating poetry environment (#10)
    * Removing dask dependency and updating poetry environment
    
    * Checking if later version of poetry works okay for installing environment
    
    * Removing unused dask import

--- tests/test_classification_pipeline.py
+++ tests/test_classification_pipeline.py
@@ -9,7 +9,7 @@ from cassa.classification_pipeline import (
     get_affinity_matrix,
     get_clusters_spectral,
 )
-from cassa.distance_matrix import compute_distance_matrix, compute_distance_matrix_dask
+from cassa.distance_matrix import compute_distance_matrix
 
 path = Path(__file__)
 
@@ -23,9 +23,6 @@ class TestDistMatrix(TestCase):
         matrix_arrays = np.random.random((100, 10, 50))
         dist_matr = compute_distance_matrix(matrix_arrays)
         self.assertEqual(matrix_arrays.shape[0], dist_matr.shape[0])
-
-        dist_matr_dask = compute_distance_matrix_dask(matrix_arrays, num_part=10)
-        self.assertTrue((dist_matr == dist_matr_dask).all())
 
         aff_matrix = get_affinity_matrix(dist_matr)
         self.assertEqual(aff_matrix.shape, dist_matr.shape)
